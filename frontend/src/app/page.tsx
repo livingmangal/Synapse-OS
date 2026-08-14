d40f8e6c717ee495d34c85b332f6d8e31eddeb81
Mausam Kar <rikikumkar@gmail.com>
2026-08-14 18:50:24 +0530

    feat: implement home page with animated SVG diagrams and add supporting media assets

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -159,7 +159,7 @@ export default function HomePage() {
         
 <div class="media mod-scroll__images__image-single noAspect noAnimate no-general-anim" data-delay=""> 
             <div class="media__wrap-source image">
-            <img class="media__source w-100" src="/wp-content/uploads/2026/04/human-anatomy.jpg" alt="Human Anatomy - Sanjeevani OS" style={{ objectFit: 'cover', height: '100%' }}>
+            <img class="media__source w-100" src="/1st-photo.webp" alt="Sanjeevani Architecture Schematic" style={{ objectFit: 'cover', width: '100%', height: '100%' }}>
         </div>
     </div>
 
@@ -169,14 +169,14 @@ export default function HomePage() {
                 
 <div class="media flipMedia__media flipMedia__media--down noAspect noAnimate no-general-anim" data-delay=""> 
             <div class="media__wrap-source image">
-            <img class="media__source w-100" src="/wp-content/uploads/2026/04/IMG_9153.jpeg.jpg">
+            <img class="media__source w-100" src="/anatomy-skull.webp" alt="Anatomical Cranial Diagram" style={{ objectFit: 'cover', width: '100%', height: '100%' }}>
         </div>
     </div>
 
 
 <div class="media flipMedia__media flipMedia__media--up noAspect noAnimate no-general-anim" data-delay=""> 
             <div class="media__wrap-source image">
-            <img class="media__source w-100" src="/wp-content/uploads/2026/04/IMG_9153.jpeg-1.jpg">
+            <img class="media__source w-100" src="/anatomy-shoulder.webp" alt="Anatomical Shoulder Diagram" style={{ objectFit: 'cover', width: '100%', height: '100%' }}>
         </div>
     </div>
 
